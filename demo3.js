cf0802ab8f987768e4fff2effedae14d1f141422
Michelle Naim <michellenaim@gmail.com>
2021-11-01 19:11:41 -0700

    Add exercise

--- demo3.js
+++ demo3.js
@@ -1,27 +1,26 @@
-  class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      
-    }
-  
-    handleSubmit(event) {
-      
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text"  />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
   }
-  
-  ReactDOM.render(
-    <NameForm />,
-    document.getElementById('root')
-  );
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.input.current.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={this.input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<NameForm />, document.getElementById("root"));
